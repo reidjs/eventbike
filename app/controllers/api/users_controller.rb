4744919e5d3febb2aac1065d0732a28df52c71e8
reid <reidsherman@gmail.com>
2017-11-17 19:20:13 -0800

    Change session controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,13 +5,10 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
-      sign_in(@user)
-      redirect_to events_url
+      render :show
     else
-      flash.now[:errors] = @user.errors.full_messages
-      render :new
+      render :errors, status: 422
     end
   end
 
