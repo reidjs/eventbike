74593b700503cc062a6d722731cf60e5347bc7ed
reid <reidsherman@gmail.com>
2017-11-24 16:04:43 -0800

    Fix messed up nav styling

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -23,3 +23,4 @@ class Api::SessionsController < ApplicationController
     end
   end
 end
+
--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Greeting = () => (
-  <div className="hero-image">
-    <div className="hero-text">
-      <p>Welcome to event bike</p>
+  <div className="hero-container">
+    <div className="hero-image">
+      <img src="http://res.cloudinary.com/eventbike/image/upload/v1511497460/joe-neric-223562_okhigx.jpg"/>
+      <div className="hero-text">
+        <p>Welcome to event bike</p>
+      </div>
     </div>
   </div>
 );
-
 export default Greeting;
