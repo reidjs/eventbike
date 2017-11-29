096c0535236978ff11b9c18e5cca4d94d4e43910
reid <reidsherman@gmail.com>
2017-11-28 22:03:20 -0800

    Adding generic events index to prepare for categories

--- frontend/components/splash/query_box.jsx
+++ frontend/components/splash/query_box.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 class QueryBox extends React.Component {
   constructor() {
     super();
@@ -8,7 +8,12 @@ class QueryBox extends React.Component {
   render() {
     return (
       <div className="hero-text">
-        <p>Welcome to EventBike</p>
+        <h1>Find your next cycling experience</h1>
+        {/* <div className="hero-categories">
+          <Link to="events/joyride">Go for a joy ride.</Link>
+          <Link to="events/race">Compete in a race.</Link>
+          <Link to="events/social">Be social.</Link>
+        </div> */}
       </div>
     )
   }
