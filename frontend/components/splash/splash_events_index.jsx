709569967b090b2f9734f09d1964efd1e28b93b6
reid <reidsherman@gmail.com>
2017-11-30 17:05:50 -0800

    Add more events button on splash

--- frontend/components/splash/splash_events_index.jsx
+++ frontend/components/splash/splash_events_index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import EventsIndexContainer from '../events/events_index_container';
 // import EventsShowContainer from '../events/events_show_container';
+import { Link } from 'react-router-dom';
 import GenericEventsShowList from '../generic_events_show_list';
 class SplashEventsIndex extends React.Component {
   constructor(props) {
@@ -24,6 +25,7 @@ class SplashEventsIndex extends React.Component {
       <div className="splash-index-container">
         <h1>Cycling events in San Francisco, California</h1>
         <GenericEventsShowList events={this.props.events}/>
+        <Link id="more-events-link" to="/events">See more events</Link>
       </div>  
     )
   }
@@ -31,114 +33,3 @@ class SplashEventsIndex extends React.Component {
 
 export default SplashEventsIndex
   
-// const SplashEventsIndex = () => {
-//   return (
-//     <div className="splash-events-container">
-//       <h1>Cycle Events in San Francisco, Calfornia</h1>
-//       <div className="flex-grid">
-//       {/* <EventsIndexContainer noSideNav={true} squares={true}/> */}
-//       {/* <div className="flex-grid"> */}
-//         <div className="col"></div>
-//         <div className="col">em ipsum dolor sit amet, 
-//       consectetur adipiscing elit,
-//        sed do eiusmod tempor incididunt
-//         ut labore et dolore magna aliqua. 
-//         Amet aliquam id di</div>
-//         <div className="col">em ipsum dolor sit amet, 
-//       consectetur adipiscing elit,
-//        sed do eiusmod tempor incididunt
-//         ut labore et dolore magna aliqua. 
-//         Amet aliquam id di</div>
-//         <div className="col">em ipsum dolor sit amet, 
-//       consectetur adipiscing elit,
-//        sed do eiusmod tempor incididunt
-//         ut labore et dolore magna aliqua. 
-//         Amet aliquam id di</div>
-//         <div className="col">ectetur adipiscing elit,
-//        sed do eiusmod tempor incididunt
-//         ut labore et dolore magna aliqua. 
-//         Amet aliquam id di</div>
-//         <div className="col">ectetur adipiscing elit,
-//        sed do eiusmod tempor incididunt
-//         ut labore et dolore magna aliqua. 
-//         Amet aliquam id di</div>
-//       </div>
-      
-//       Lorem ipsum dolor sit amet, 
-//       consectetur adipiscing elit,
-//        sed do eiusmod tempor incididunt
-//         ut labore et dolore magna aliqua. 
-//         Amet aliquam id diam maecenas ult
-//         ricies mi. Sed sed risus pretium 
-//         quam vulputate dignissquam id diam maecenas ult
-//         ricies mi. Sed sed risus pretium 
-//         quam vulputate dignissim suspendi
-//         sse. Odio pellentesque diam volutpa
-//         t commodo. Senectus et netus et male
-//         suada fames. Turpis in eu mi bibendu
-//         m. In ante metus dictum at tempor com
-//         modo ullamcorper. Et netus et malesua
-//         da fames. Amet nisl purus in
-//          mollis nunc sed id semper risus. P
-//          orttitor rhoncus dolor purus non e
-//          nim praesent elementum facilisis. Sit amet riquam id diam maecenas ult
-//         ricies mi. Sed sed risus pretium 
-//         quam vulputate dignissim suspendi
-//         sse. Odio pellentesque diam volutpa
-//         t commodo. Senectus et netus et male
-//         suada fames. Turpis in eu mi bibendu
-//         m. In ante metus dictum at tempor com
-//         modo ullamcorper. Et netus et malesua
-//         da fames. Amet nisl purus in
-//          mollis nunc sed id semper risus. P
-//          orttitor rhoncus dolnunc sed id semper risus. P
-//          orttitor rhoncus dolor purus non e
-//          nim praesent elementum facilisis. Sit amet riquam id diam maecenas ult
-//         ricies mi. Sed sed risus pretium 
-//         quam vulputate dignissim suspendi
-//         sse. Odio pellentesque diam volutpa
-//         t commodo. Senectus et netus et male
-//         suada fames. Turpis in eu mi bibendu
-//         m. In ante metus dictum at tempor com
-//         modo ullamcorper. Et netus et malesua
-//         da fames. Amet nisl purus in
-//          mollis nunc sed id semper risus. P
-//          orttitor rhoncus dolnunc sed id semper risus. P
-//          orttitor rhoncus dolor purus non e
-//          nim praesent elementum facilisis. Sit amet riquam id diam maecenas ult
-//         ricies mi. Sed sed risus pretium 
-//         quam vulputate dignissim suspendi
-//         sse. Odio pellentesque diam volutpa
-//         t commodo. Senectus et netus et male
-//         suada fames. Turpis in eu mi bibendu
-//         m. In ante metus dictum at tempor com
-//         modo ullamcorper. Et netus et malesua
-//         da fames. Amet nisl purus in
-//          mollis nunc sed id semper risus. P
-//          orttitor rhoncus dolnunc sed id semper risus. P
-//          orttitor rhoncus dolor purus non e
-//          nim praesent elementum facilisis. Sit amet riquam id diam maecenas ult
-//         ricies mi. Sed sed risus pretium 
-//         quam vulputate dignissim suspendi
-//         sse. Odio pellentesque diam volutpa
-//         t commodo. Senectus et netus et male
-//         suada fames. Turpis in eu mi bibendu
-//         m. In ante metus dictum at tempor com
-//         modo ullamcorper. Et netus et malesua
-//         da fames. Amet nisl purus in
-//          mollis nunc sed id semper risus. P
-//          orttitor rhoncus dolor purus non e
-//          nim praesent elementum facilisis. Sit amet riim suspendi
-//         sse. Odio pellentesque diam volutpa
-//         t commodo. Senectus et netus et male
-//         suada fames. Turpis in eu mi bibendu
-//         m. In ante metus dictum at tempor com
-//         modo ullamcorper. Et netus et malesua
-//         da fames. Amet nisl purus in
-//          mollis nunc sed id semper risus. P
-//          orttitor rhoncus dolor purus non e
-//          nim praesent elementum facilisis. Sit amet risus nullam eget felis eget nunc lobortis mattis. Vivamus arcu felis bibendum ut tristique. Sed arcu non odio euismod lacinia at quis risus sed. Gravida cum sociis natoque penatibus et. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros in. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis. Lacus sed turpis tincidunt id aliquet risus feugiat.
-//     </div>
-//   )
-// }
-
